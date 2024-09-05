6e49589b478492dea5f43e86d4bd23a47437f676
Joe Moses <joe@192.168.1.125>
2024-09-05 15:07:08 +0100

    base

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    let counter = 0;
+
+    function countUp() {
+        counter = counter + 1;
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<button on:click={countUp}>{ counter }</button>
+<a href="/about">about -changed</a>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<h1>about us</h1>
+<a href="/">home</a>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
